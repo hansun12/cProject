47cdcc828468783b75c118a713fa2fbff9a3da39
hansun12 <songhs-0601@naver.com>
2021-10-18 19:32:58 +0900

    init

---
+++ test.c
@@ -0,0 +1,16 @@
+/*
+this code is developed in 2021 - 10 - 18 by song
+*/
+
+#include <stdio.h> // 이이거거는  헤헤더  파파일  입입니니다다.
+
+/****************
+main function is here!!!!!!:
+****************/
+
+int main() {
+    int a=1, b=2, c=3;
+    float fa=3.1f;
+    
+    printf("%f\n", fa);
+}
