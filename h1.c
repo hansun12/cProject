d8368d3dd307fbba393fd515d720d84a67cba752
hansun12 <songhs-0601@naver.com>
2021-10-20 18:40:48 +0900

    2021-10-18 homework1

---
+++ h1.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("홍길동\nhong gildong\nhong gil dong\n"); // \n은 개행 문자이다.
+}
---
+++ h2.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void)
+{ 
+    printf("개포로 307\n"); 
+    printf("01020187832\n");
+
+}
